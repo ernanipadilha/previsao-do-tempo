c47626d2e1ce5e267aa0f9052c9a0bfd8238d2be
ernanipadilha <ernanipadilha@gmail.com>
2022-01-27 19:55:47 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+//https://apiprevmet3.inmet.gov.br/previsao/capitais
 
-function App() {
+function App(props) {
+  const [estados, setEstados] = useState([])
+  //const []
+  useEffect(() => {
+    fetch(
+      'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome',
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setEstados(data)
+      })
+  }, [])
+
+  function getEstado(e) {
+    const estadoId = e
+    console.log(estadoId.value)
+    return estadoId.value
+  }
+
+  function getMunicipio(e) {
+    const estadoId = getEstado(e)
+    fetch(
+      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="estado">Selecione o estado: </label>
+      <select onChange={(e) => getEstado({ value: e.target.value })}>
+        {estados.map((estados) => (
+          <option key={estados.id} value={estados.id}>
+            {estados.nome}
+          </option>
+        ))}
+      </select>
+      <label for="municipio">Selecione o municipio: </label>
+      <select>
+        {estados.map((estados) => (
+          <option key={estados.id} value={estados.id}>
+            {estados.nome}
+          </option>
+        ))}
+      </select>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
