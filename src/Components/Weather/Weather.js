aa2c5dca2faa26202537e0f4727757e11ca117cf
ernanipadilha <ernanipadilha@gmail.com>
2022-02-07 16:55:39 -0300

    Ajustes de layout

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -25,31 +25,23 @@ export function Weather({ id }) {
   return (
     <main>
       {days.map((day) => (
-        <Box
-          
-          className="card"
-          sx={{
-            width: 300,
-          }}
-        >
-            <Paper className="resume" elevation={3} sx={{m:1}}>
-          {day.data} - {day?.manha?.entidade || day.entidade}
-          <Image
-            className="icon-temp"
-            shift="center"
-            fit="contain"
-            width="50%"
-            heigth="30%"
-            src={day?.manha?.icone || day.icone}
-          ></Image>
-          Temperatura máxima: {day?.manha?.temp_max || day.temp_max}ºC
-          <br />
-          Temperatura mínima: {day?.manha?.temp_min || day.temp_min}ºC
-          <br />
-          Intensidade do vento: {day?.manha?.int_vento || day.int_vento}
-          <br />
-          Resumo do dia: {day?.manha?.resumo || day.resumo} <br />
-          Estação do ano: {day?.manha?.estacao || day.estacao}
+        <Box className="card" sx={{ width: 350, height: 350 }}>
+          <Paper className="resume" elevation={3} sx={{ m: 1 }}>
+            {day.data} - {day?.manha?.entidade || day.entidade}
+            <Image
+              className="icon-temp"
+              height="50%"
+              width="50%"
+              src={day?.manha?.icone || day.icone}
+            ></Image>
+            Temperatura máxima: {day?.manha?.temp_max || day.temp_max}ºC
+            <br />
+            Temperatura mínima: {day?.manha?.temp_min || day.temp_min}ºC
+            <br />
+            Intensidade do vento: {day?.manha?.int_vento || day.int_vento}
+            <br />
+            Resumo do dia: {day?.manha?.resumo || day.resumo} <br />
+            Estação do ano: {day?.manha?.estacao || day.estacao}
           </Paper>
         </Box>
         /* <p>
