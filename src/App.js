8b052c2f9b1387d418c0563d889a625cc1b3969c
ernanipadilha <ernanipadilha@gmail.com>
2022-02-07 15:02:40 -0300

    Ajustes para responsividade

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css'
-import { Box } from '@mui/material'
 import { State } from './Components/State/State'
 //https://apiprevmet3.inmet.gov.br/previsao/5300108
 
 function App() {
   return (
     <div className="App">
-      <h1>Previsão do Tempo ⛅</h1>
-      <Box className="teste">
+      <h1 className='title'>Previsão do Tempo ⛅</h1>
+      
         <State></State>
-      </Box>
+      
     </div>
   )
 }
